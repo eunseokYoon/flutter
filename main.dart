2c827a17d8698a28b76cd3c8cdcb37a5cc62fb2a
yoon <dmstjrwltjr@naver.com>
2024-12-27 19:27:23 +0900

    1

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(){
+  Map dictionary = {
+    'Harry Potter' : '해리포터',
+    'Ron Weasley' : '론 위즐리',
+  };
+
+  Map<String, String> dictionary2 = {
+    'Harry Potter' : '해리포터',
+    'Ron Weasley' : '론 위즐리',
+  };
+}
